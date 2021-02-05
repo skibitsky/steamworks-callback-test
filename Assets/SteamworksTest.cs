1526e379411b0b02ff1b1ab1c119f0b8527db378
skibitsky <gleb@skibitsky.com>
2021-02-05 11:44:49 +0200

    Project created

---
+++ Assets/SteamworksTest.cs
@@ -0,0 +1,15 @@
+﻿using Steamworks;
+using UnityEngine;
+
+public class SteamworksTest : MonoBehaviour
+{
+    [SerializeField] private uint SteamAppId;
+
+    private void Start()
+    {
+        SteamClient.Init(SteamAppId);
+
+        SteamFriends.OnGameOverlayActivated +=
+            active => Debug.Log($"[Steamworks] OnGameOverlayActivated. Active: {active}");
+    }
+}
